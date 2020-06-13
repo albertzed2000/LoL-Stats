1a1123e3625b149aa13009d9e5c31eea6ed68dcd
Albert Zheng <mralbertzheng@gmail.com>
2020-06-13 02:46:07 -0700

    playing around to figure out cors problem

--- lol-stats/src/components/IndexPage.js
+++ lol-stats/src/components/IndexPage.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-
 export default class IndexPage extends Component {
 
     constructor(props){
@@ -47,18 +46,7 @@ export default class IndexPage extends Component {
 
     async onSubmit(e){
         e.preventDefault();
-        //this.checkUser();
-
-        await axios.get("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/albertzed?api_key=RGAPI-dc9629ff-9e1c-4f16-a07c-05cc83cbfe8a", {
-            headers: {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
-                "Accept-Language": "en-US,en;q=0.9",
-                "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-                "Origin": "localhost:3000",
-                "X-Riot-Token": "RGAPI-dc9629ff-9e1c-4f16-a07c-05cc83cbfe8a"
-            }
-        })
-        .then(res => console.log(res))
+        this.checkUser();
         
 
         if(this.state.foundUser){
@@ -88,7 +76,6 @@ export default class IndexPage extends Component {
 
                 <input type="submit" value="Submit" />
                 </form>
-                {this.state.yeet}
 
             </div>
 
