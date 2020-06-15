9c904068efc2e3d16640fda08efaa09d45311ba1
Albert Zheng <albert.zheng@mail.utoronto.ca>
2020-06-15 01:34:29 -0700

    send api req to proxy and display rank

--- lol-stats/src/components/UserStats.js
+++ lol-stats/src/components/UserStats.js
@@ -13,20 +13,29 @@ export default class UserStats extends Component {
             foundUser: false,
             username: this.props.match.params.username,
             accountId: "",
+            summonerId: "",
             summonerLevel: 0,
+            soloRank: "",
+            soloTier: "",
+            flexRank: "",
+            flexTier: "",
+
 
         }
     }
 
     async componentDidMount(){
         //load stats into state by calling api proxy server
+
+        //load base user stats by sending getSummonerById api request
         await axios.get('https://m6m1r9620d.execute-api.us-west-2.amazonaws.com/rgapi/summoner/na1/' + this.state.username)
         .then(res => {
             console.log(res.data["accountId"]); //debugging purposes
             this.setState({
-                foundUser: true,
-                accountId: res.data["accountId"],
-                summonerLevel: res.data["summonerLevel"]
+                foundUser: true, //set founduser to true
+                accountId: res.data["accountId"], //set accountId based on response
+                summonerLevel: res.data["summonerLevel"], //set summonerLevel based on response
+                summonerId: res.data["id"] //set summonerId based on response
                 }); //set state to accountId
         })
         .catch((err) => {
@@ -37,6 +46,19 @@ export default class UserStats extends Component {
         if(!this.state.foundUser){
             window.location = "/user-not-found/" + this.state.username;
         }
+
+
+        //load summoner rank using getLeagueBySummonerId api request
+        await axios.get("https://m6m1r9620d.execute-api.us-west-2.amazonaws.com/rgapi/rank/na1/" + this.state.summonerId)
+        .then(res => {
+            console.log(res.data[0]);
+            this.setState({
+                flexTier: res.data[0]["tier"],
+                flexRank: res.data[0]["rank"],
+                soloTier: res.data[1]["tier"],
+                soloRank: res.data[0]["rank"],
+            })
+        })
     }
 
     render(){
@@ -46,8 +68,11 @@ export default class UserStats extends Component {
         <div>
         {this.state.username} <br/>
         {this.state.foundUser}<br/>
-        {this.state.summonerLevel}<br/>
+        Level: {this.state.summonerLevel}<br/>
         {this.state.accountId}<br/>
+        {this.state.summonerId}<br/>
+        Flex rank: {this.state.flexTier +  " " + this.state.flexRank}<br/>
+        Solo/duo rank: {this.state.soloTier + " " + this.state.soloRank}
         </div>
 
         )
