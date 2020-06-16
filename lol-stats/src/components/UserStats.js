1d266ceac744678ad9a380cf15db248ea7fc8b9c
Albert Zheng <albert.zheng@mail.utoronto.ca>
2020-06-15 18:46:46 -0700

    store recent matchdata in state

--- lol-stats/src/components/UserStats.js
+++ lol-stats/src/components/UserStats.js
@@ -11,16 +11,17 @@ export default class UserStats extends Component {
         //initialize state empty except for encryptedSummonerId passed in through url
         this.state = {
             foundUser: false,
-            username: this.props.match.params.username,
-            accountId: "",
-            summonerId: "",
-            summonerLevel: 0,
-            soloRank: "",
-            soloTier: "",
-            flexRank: "",
-            flexTier: "",
-            matches: [],
-
+            username: this.props.match.params.username, // summoner's username
+            accountId: "",  //summoner's encryptedAccountId
+            summonerId: "", //encryptedSummonerId
+            summonerLevel: 0,   //in-game experience-based level rating
+            soloRank: "",   // summoner solo/duo queue rank (eg. III)
+            soloTier: "",   //summoner solo/duo tier (eg. silver)
+            flexRank: "",   // summoner flex queue rank (eg. III)
+            flexTier: "",   //summoner flex queue tier (eg. silver)
+            matches: [],    //array of objects of basic match data (used to prepare loading in match data)
+            matchData: [],  //array of objects, each containing in-depth info of each recent match summoner has played. contains object
+            
 
         }
     }
@@ -72,7 +73,7 @@ export default class UserStats extends Component {
             
             if(res.data["matches"].length >=5){
                     this.setState({
-                    matches: res.data["matches"].slice(0, 5), //get most recent 5 matches only (due to api rate limiting)
+                    matches: res.data["matches"].slice(0, 1), //get most recent 5 matches only (due to api rate limiting)
                 })
             }
             else{ //if user has played less than 5 games, store all of them in this.state.matches
@@ -82,19 +83,72 @@ export default class UserStats extends Component {
             }
         })
 
+
+        //retrieve match info for each match inside this.state.matches
         await this.state.matches.forEach((match) => {
             axios.get('https://m6m1r9620d.execute-api.us-west-2.amazonaws.com/rgapi/match/na1/' + match["gameId"])
             .then(res => {
-                console.log(res.data);
+                console.log(res.data); //debugging purposes
+                
+                let tempMatchData = { //store general game data here
+                    "region": match["platformId"], //string region
+                    "matchId": match["gameId"], // string match id
+                    "champion": match["champion"], // int champion that our summoner picked
+                    "timestamp": match["timestamp"], //int unix gamestart timestamp
+                    "duration": res.data["gameDuration"], //int, how long the game lasted in seconds, counted from 0:00 (gamestart)
+                    "queueId": res.data["queueId"], //int, matchtype id
+                    "mapId": res.data["mapId"], // int, map id that was played on 
+                }
 
-            });
-        });
 
+                var tempParticipantId = 0;
+                //loop through participantIdentities and find our player
+                res.data["participantIdentities"].forEach((participantIdentity) => {
+                    if(participantIdentity["player"]["summonerName"] === this.state.username){ //check if we have found our summoner
+                        console.log("found our summoner with username" + participantIdentity["player"]["summonerName"] + "and participantId " + participantIdentity["participantId"]) //debugging purposes
+                        tempParticipantId = participantIdentity["participantId"]; // set player's participantId
+                    }
+                });
+
+                //note: "summoner" and "player" terms are the same.
+                //loop through participant stats, record stats if it is our summoner
+                res.data["participants"].forEach((participant) => {
+                    if(participant["participantId"] === tempParticipantId){ // check if we have found our player
+                        console.log("we found the player")//debugging purposes
+
+                        let tempPlayerStats = { // store playerStats
+                            "win": participant["stats"]["win"],
+                            "kills": participant["stats"]["kills"],
+                            "deaths": participant["stats"]["deaths"],
+                            "assists": participant["stats"]["assists"],
+                            "item0": participant["stats"]["item0"],
+                            "item1": participant["stats"]["item1"],
+                            "item2": participant["stats"]["item2"],
+                            "item3": participant["stats"]["item3"],
+                            "item4": participant["stats"]["item4"],
+                            "item5": participant["stats"]["item5"],
+                            "item6": participant["stats"]["item6"],
+                        };
+                        console.log(tempPlayerStats);
+                        console.log(tempMatchData);
+                        console.log({...tempMatchData, ...tempPlayerStats});
+                        this.setState({
+                            matchData: this.state.matchData.concat([{...tempMatchData, ...tempPlayerStats}])
+                        })
+                    }
+                });
+
+
+            })
+            .catch(err => console.log(err));
+            
+        })
+        
+        
         console.log(this.state.matches); //debugging purposes
     }
 
     render(){
-
 
         return(
         <div>
